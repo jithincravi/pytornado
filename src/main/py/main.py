ae6140fd3270e875f1ccfde09e69ed1eeba405df
jithincravi <jithin.rocks@gmail.com>
2021-09-15 17:04:08 -0400

    Initial Push

--- src/main/py/main.py
+++ src/main/py/main.py
@@ -5,6 +5,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
